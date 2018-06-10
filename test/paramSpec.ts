1e701fd9feeb1cbadcd0aca1e4ce4117e4b55549
Chris Thielen <cthielen@netflix.com>
2018-06-10 09:30:11 -0700

    test(params): Added failing test for https://github.com/ui-router/core/issues/178

--- test/paramSpec.ts
+++ test/paramSpec.ts
@@ -1,4 +1,6 @@
-import { ParamTypes } from '../src/index';
+import { ParamTypes, UIRouter } from '../src/index';
+import { ParamTypeDefinition } from '../src/params';
+import { StateObject } from '../src/state';
 
 describe('parameters', () => {
   let types;
@@ -30,6 +32,90 @@ describe('parameters', () => {
       // Failing test case for #2484
       expect(dateType.equals(date1, date2)).toBeTruthy();
       expect(dateType.equals(date1, date3)).toBeFalsy();
+    });
+  });
+
+  describe('from a custom type', () => {
+    let router: UIRouter = null;
+    let state: StateObject = null;
+
+    const customTypeA: ParamTypeDefinition = {
+      encode: val => (val ? 'true' : 'false'),
+      decode: str => (str === 'true' ? true : str === 'false' ? false : undefined),
+      dynamic: false,
+      equals: (a, b) => a === b,
+      inherit: true,
+      is: val => typeof val === 'boolean',
+      pattern: /(?:true|false)/,
+      raw: true,
+    };
+
+    const customTypeB: ParamTypeDefinition = {
+      encode: val => (val ? 'true' : 'false'),
+      decode: str => (str === 'true' ? true : str === 'false' ? false : undefined),
+      dynamic: true,
+      equals: (a, b) => a === b,
+      inherit: false,
+      is: val => typeof val === 'boolean',
+      pattern: /(?:true|false)/,
+      raw: false,
+    };
+
+    describe('as a simple path parameter', () => {
+      beforeEach(() => {
+        router = new UIRouter();
+        router.urlService.config.type('customTypeA', customTypeA);
+        router.urlService.config.type('customTypeB', customTypeB);
+
+        state = router.stateRegistry.register({
+          name: 'state',
+          url: '/{paramA:customTypeA}/{paramB:customTypeB}',
+        });
+      });
+
+      it('should use `dynamic` from the custom type', () => {
+        expect(state.parameter('paramA').dynamic).toEqual(customTypeA.dynamic);
+        expect(state.parameter('paramB').dynamic).toEqual(customTypeB.dynamic);
+      });
+
+      it('should use `inherit` from the custom type', () => {
+        expect(state.parameter('paramA').inherit).toEqual(customTypeA.inherit);
+        expect(state.parameter('paramB').inherit).toEqual(customTypeB.inherit);
+      });
+
+      it('should use `raw` from the custom type', () => {
+        expect(state.parameter('paramA').raw).toEqual(customTypeA.raw);
+        expect(state.parameter('paramB').raw).toEqual(customTypeB.raw);
+      });
+    });
+
+    describe('as an array path parameter', () => {
+      beforeEach(() => {
+        router = new UIRouter();
+        router.urlService.config.type('customTypeA', customTypeA);
+        router.urlService.config.type('customTypeB', customTypeB);
+
+        state = router.stateRegistry.register({
+          name: 'state',
+          url: '/{paramA[]:customTypeA}/{paramB[]:customTypeB}',
+        });
+      });
+
+      it('should use `dynamic` from the custom type', () => {
+        expect(state.parameter('paramA[]').dynamic).toEqual(customTypeA.dynamic);
+        expect(state.parameter('paramB[]').dynamic).toEqual(customTypeB.dynamic);
+      });
+
+      it('should use `inherit` from the custom type', () => {
+        expect(state.parameter('paramA[]').inherit).toEqual(customTypeA.inherit);
+        expect(state.parameter('paramB[]').inherit).toEqual(customTypeB.inherit);
+      });
+
+      // Test for https://github.com/ui-router/core/issues/178
+      it('should use `raw` from the custom type', () => {
+        expect(state.parameter('paramA[]').raw).toEqual(customTypeA.raw);
+        expect(state.parameter('paramB[]').raw).toEqual(customTypeB.raw);
+      });
     });
   });
 });
